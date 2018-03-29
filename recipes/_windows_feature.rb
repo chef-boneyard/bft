1cd8239903e10738ae4043d326fcdbb5ba3f8f0f
Tim Smith <tsmith@chef.io>
2018-03-28 17:48:15 -0700

    Don't test windows_feature_powershell on windows 2008 R2
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/_windows_feature.rb
+++ recipes/_windows_feature.rb
@@ -11,26 +11,24 @@ windows_feature 'Install SNMP again' do
   feature_name ['SNMP']
 end
 
-# This is for appveyor, which already seems to have FTP installed
-# which causes a short circuit of the "all" behavior and breaks the test.
-# TODO: Make :windows_feature_powershell look at all the sub-features and validate
-# that they are installed when "all is specified"
-windows_feature 'Remove FTP for Appveyor' do
-  feature_name 'Web-Ftp-Server'
-  action :remove
-  install_method :windows_feature_powershell
-end
-
-windows_feature 'Web-Ftp-Server' do
-  all true
-  install_method :windows_feature_powershell
-end
-
 windows_feature_dism %w(TelnetClient TFTP)
 
-windows_feature_powershell ['Web-Asp-Net45', 'Web-Net-Ext45']
+unless node['platform_version'].to_f < 6.2
+  windows_feature 'Remove FTP for Appveyor' do
+    feature_name 'Web-Ftp-Server'
+    action :remove
+    install_method :windows_feature_powershell
+  end
 
-windows_feature ['NPAS'] do
-  management_tools true
-  install_method :windows_feature_powershell
+  windows_feature 'Web-Ftp-Server' do
+    all true
+    install_method :windows_feature_powershell
+  end
+
+  windows_feature_powershell ['Web-Asp-Net45', 'Web-Net-Ext45']
+
+  windows_feature ['NPAS'] do
+    management_tools true
+    install_method :windows_feature_powershell
+  end
 end
