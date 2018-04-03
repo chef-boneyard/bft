d08eca5ab3a409d4e124b34603b198dde4bd51b9
Tim Smith <tsmith@chef.io>
2018-04-02 21:37:41 -0400

    Add homebrew tests
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ recipes/_homebrew.rb
@@ -0,0 +1,19 @@
+homebrew_tap 'homebrew/dupes'
+
+homebrew_tap 'homebrew/dupes' do
+  action :untap
+end
+
+homebrew_tap "Let's install homebrew/dupes" do
+  tap_name 'homebrew/dupes'
+  url 'https://github.com/homebrew/homebrew-dupes.git'
+  full true
+end
+
+homebrew_cask 'google-chrome'
+
+homebrew_cask "Let's remove google-chrome" do
+  cask_name 'google-chrome'
+  install_cask false
+  action :remove
+end
