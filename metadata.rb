e5db5af17aeb87b3d496977113b4409f43460257
Tim Smith <tsmith@chef.io>
2018-04-02 21:34:28 -0400

    Update metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,9 @@ maintainer_email 'tsmith@chef.io'
 license 'Apache-2.0'
 description 'Test resources in core Chef'
 version '1.0'
+%w(ubuntu debian redhat centos suse opensuse opensuseleap scientific oracle amazon).each do |os|
+  supports os
+end
+source_url 'https://github.com/tas50/bft'
+issues_url 'https://github.com/tas50/bft/issues'
 chef_version '>= 14'
