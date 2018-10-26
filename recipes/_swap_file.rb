bc10c3e7d9450b4f359cfcaa5520b79f11510622
Tim Smith <tsmith@chef.io>
2018-10-25 17:04:35 -0700

    Skip SLES entirely for swap_file
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/_swap_file.rb
+++ recipes/_swap_file.rb
@@ -1,5 +1,5 @@
 swap_file '/mnt/swap' do
   size 10
   persist true
-  not_if { node['filesystem']['by_mountpoint']['/']['fs_type'] == 'btrfs' }
+  not_if { platform_family?('suse') } # sles / opensuse filesystems don't work with swap files
 end
