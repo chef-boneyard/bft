976af397a1387f97347cc12f98dde53a10d0e60e
Tim Smith <tsmith@chef.io>
2018-10-31 09:20:52 -0700

    Add windows_certificate testing
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/windows.rb
+++ recipes/windows.rb
@@ -8,6 +8,7 @@ end
 windows_workgroup 'myworkgroup'
 
 include_recipe '::_windows_autorun'
+include_recipe '::_windows_certificate'
 include_recipe '::_windows_feature'
 include_recipe '::_windows_font'
 include_recipe '::_windows_pagefile'
