0ae9e4f8ea4b1de5c5a30a4d20a7f58d98b50735
Tim Smith <tsmith@chef.io>
2018-11-02 09:58:16 -0700

    Add windows_firewall testing
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ recipes/_windows_firewall.rb
@@ -0,0 +1,27 @@
+windows_firewall_rule 'Apache' do
+  local_port '8080'
+  protocol 'TCP'
+  firewall_action :allow
+  program 'c:\program files\test.exe'
+  local_address '192.168.1.1'
+end
+
+windows_firewall_rule 'Disable WinRM over HTTP' do
+  local_port '5985'
+  protocol 'TCP'
+  firewall_action :block
+  program 'c:\program files\test.exe'
+  local_address '192.168.1.1'
+end
+
+windows_firewall_rule 'Enable SSH Access' do
+  rule_name 'ssh'
+  local_port '22'
+  protocol 'TCP'
+  firewall_action :allow
+end
+
+windows_firewall_rule 'Remove SSH Access' do
+  rule_name 'ssh'
+  action :delete
+end
