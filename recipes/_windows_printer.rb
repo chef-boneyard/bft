abe90a6fc1065d18b3fd5ab9e657fe027c92d816
Tim Smith <tsmith@chef.io>
2018-11-13 19:33:01 -0800

    Add basic windows_printer test
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/_windows_printer.rb
+++ recipes/_windows_printer.rb
@@ -1,1 +1,8 @@
-# nothing yet
+# windows_printer 'HP LaserJet 5th Floor' do
+#   driver_name 'HP LaserJet 4100 Series PCL6'
+#   ipv4_address '10.4.64.38'
+# end
+
+windows_printer 'HP LaserJet 6th Floor' do
+  action :delete
+end
