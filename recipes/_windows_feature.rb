bd060c75389bd903394a07171bb707ab773d3153
Tim Smith <tsmith84@gmail.com>
2020-05-07 12:21:23 -0700

    Remove old 2008 R2 support
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/_windows_feature.rb
+++ recipes/_windows_feature.rb
@@ -8,32 +8,25 @@ windows_feature 'remove SNMP' do
 end
 
 windows_feature 'Install SNMP again' do
-  feature_name node['platform_version'].to_f < 6.2 ? 'SNMP' : 'snmp' # lowercase on purpose
+  feature_name 'snmp' # lowercase on purpose
 end
 
 windows_feature_dism %w(TelnetClient TFTP)
 
-# Powershell in windows 2008r2 is old and awful. users need to upgrade
-unless node['platform_version'].to_f < 6.2 || node['kernel']['product_type'] == 'Workstation'
-  # This is for appveyor, which already seems to have FTP installed
-  # which causes a short circuit of the "all" behavior and-breaks the test.
-  # TODO: Make :windows_feature_powershell look at all the sub-features and validate
-  # that they are installed when "all is specified"
-  windows_feature 'Remove FTP for Appveyor' do
-    feature_name 'Web-Ftp-Server'
-    action :remove
-    install_method :windows_feature_powershell
-  end
+windows_feature 'Remove FTP' do
+  feature_name 'Web-Ftp-Server'
+  action :remove
+  install_method :windows_feature_powershell
+end
 
-  windows_feature 'web-ftp-server' do # lowercase on purpose
-    all true
-    install_method :windows_feature_powershell
-  end
+windows_feature 'web-ftp-server' do # lowercase on purpose
+  all true
+  install_method :windows_feature_powershell
+end
 
-  windows_feature_powershell %w(Web-Asp-Net45 Web-Net-Ext45)
+windows_feature_powershell %w(Web-Asp-Net45 Web-Net-Ext45)
 
-  windows_feature ['NPAS'] do
-    management_tools true
-    install_method :windows_feature_powershell
-  end
+windows_feature ['NPAS'] do
+  management_tools true
+  install_method :windows_feature_powershell
 end
