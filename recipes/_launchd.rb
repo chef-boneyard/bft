1574b14652b5e01deacd02daf10b25dd89f6415a
Tim Smith <tsmith84@gmail.com>
2020-05-13 16:30:54 -0700

    Add basic launchd test
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ recipes/_launchd.rb
@@ -0,0 +1,4 @@
+launchd 'com.googlecode.munki.app_usage_monitor' do
+  type 'daemon'
+  action :delete
+end
--- recipes/macos.rb
+++ recipes/macos.rb
@@ -1,3 +1,4 @@
 include_recipe '::_homebrew'
 include_recipe '::_dmg'
 include_recipe '::_userdefaults'
+include_recipe '::_launchd'
